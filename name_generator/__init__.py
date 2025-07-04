e60ea6afb950c13f9f67cbda57b330c8af5784bd
吴子豪 <vortez.wohl@gmail.com>
2025-07-04 14:24:21 +0800

    feat: 实现语言包下载逻辑

---
+++ name_generator/__init__.py
@@ -0,0 +1,3 @@
+from .name_generator import NameGenerator
+from .enum.country import Country
+from .enum.gender import Gender
--- name_generator/name_generator.py
+++ name_generator/name_generator.py
@@ -2,18 +2,33 @@ import json
 import os
 import random
 
+import gdown
+import requests
+
 from name_generator.enum.country import Country
 from name_generator.enum.gender import Gender
 from name_generator.resource import __PATH__
 
 
-class Generator(object):
+class NameGenerator(object):
     def __init__(self, country: Country):
         self._country = country
         self._resource_path = os.path.join(__PATH__, f'{self._country.numeric}.json')
         self._resource = None
         if not os.path.exists(self._resource_path):
-            ...
+            url = f'https://github.com/vortezwohl/NameGenerator/releases/download/0.0.0/{self._country.numeric}.json'
+            if requests.get(url=url).status_code == 200:
+                try:
+                    gdown.download(
+                        url=url,
+                        output=self._resource_path,
+                        quiet=True,
+                        proxy=os.getenv('HTTPS_PROXY', os.getenv('https_proxy', None))
+                    )
+                except:
+                    os.remove(self._resource_path)
+            else:
+                raise ValueError(f'{self._country.official_name} is currently not supported.')
         with open(self._resource_path, 'r', encoding='utf-8') as r:
             self._resource = json.load(r)
 
@@ -26,8 +41,3 @@ class Generator(object):
         if surname_first:
             first_name, last_name = last_name, first_name
         return f'{first_name}{segment}{last_name}'
-
-
-if __name__ == '__main__':
-    for _ in range(100000):
-        print(Generator(Country.USA).next_name(Gender.Male, surname_first=False, hyphenate=True), end=', ')
