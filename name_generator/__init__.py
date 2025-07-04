b486a5cf6608896713627a20ce203866501f65fc
吴子豪 <vortez.wohl@gmail.com>
2025-07-04 14:26:58 +0800

    update: project

--- name_generator/__init__.py
+++ name_generator/__init__.py
@@ -1,3 +1,9 @@
 from .name_generator import NameGenerator
 from .enum.country import Country
 from .enum.gender import Gender
+
+__AUTHOR__ = '吴子豪 / Vortez Wohl'
+__EMAIL__ = 'vortez.wohl@gmail.com'
+__VERSION__ = '0.0.1'
+__GITHUB__ = 'https://github.com/vortezwohl'
+__BLOG__ = 'https://vortezwohl.github.io'
