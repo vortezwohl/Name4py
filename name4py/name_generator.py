d5c91ee34db0d833f3e0d518cc4ec87a0daab540
吴子豪 <vortez.wohl@gmail.com>
2025-07-05 15:23:27 +0800

    feat: 支持批量生成, 支持代理

--- name4py/name_generator.py
+++ name4py/name_generator.py
@@ -4,6 +4,8 @@ import random
 
 import gdown
 import requests
+from vortezwohl.random import next_seed
+from vortezwohl.concurrent import ThreadPool
 
 from name4py.enum.country import Country
 from name4py.enum.gender import Gender
@@ -15,15 +17,19 @@ class NameGenerator(object):
         self._country = country
         self._resource_path = os.path.join(__PATH__, f'{self._country.numeric}.json')
         self._resource = None
+        self.__proxies = {
+            'http': os.getenv('HTTP_PROXY', os.getenv('http_proxy')),
+            'https': os.getenv('HTTPS_PROXY', os.getenv('https_proxy'))
+        }
         if not os.path.exists(self._resource_path):
             url = f'https://github.com/vortezwohl/Name4py/releases/download/0.0.0/{self._country.numeric}.json'
-            if requests.get(url=url).status_code == 200:
+            if requests.get(url=url, proxies=self.__proxies).status_code == 200:
                 try:
                     gdown.download(
                         url=url,
                         output=self._resource_path,
                         quiet=True,
-                        proxy=os.getenv('HTTPS_PROXY', os.getenv('https_proxy', None))
+                        proxy=self.__proxies.get('https')
                     )
                 except:
                     os.remove(self._resource_path)
@@ -32,7 +38,17 @@ class NameGenerator(object):
         with open(self._resource_path, 'r', encoding='utf-8') as r:
             self._resource = json.load(r)
 
-    def next_name(self, gender: Gender, family_name: str | None = None, surname_first: bool = False, hyphenate: bool = True):
+    def __call__(self, **kwargs) -> str | list[str]:
+        if 'batch_size' in kwargs.keys():
+            return self.batch_generate(**kwargs)
+        return self.generate(**kwargs)
+
+    def generate(self, gender: Gender, family_name: str | None = None, surname_first: bool = False,
+                 hyphenate: bool = True, seed: int | None = None) -> str:
+        if seed:
+            random.seed(seed)
+        else:
+            next_seed()
         first_name = random.sample(self._resource['first_name'][gender.value], k=1)[0]
         last_name = random.sample(self._resource['last_name'], k=1)[0] if not family_name else family_name
         while first_name in last_name:
@@ -41,3 +57,11 @@ class NameGenerator(object):
         if surname_first:
             first_name, last_name = last_name, first_name
         return f'{first_name}{segment}{last_name}'
+
+    def batch_generate(self, batch_size: int, gender: Gender, family_name: str | None = None,
+                       surname_first: bool = False, hyphenate: bool = True, seed: int | None = None) -> list[str]:
+        threads = ThreadPool()
+        results = threads.map(jobs=[self.generate] * batch_size,
+                              arguments=[(gender, family_name, surname_first, hyphenate, seed)] * batch_size)
+        results = [_[2] for _ in results if len(_) > 2]
+        return results
